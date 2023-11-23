6bda734f258ab2cd87613c3027f194dffecb8abb
DAVV422 <diegoalberto42216@gmail.com>
2023-11-23 08:48:17 -0400

    carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { RegistrarComponent } from './pages/registrar/registrar.component';
+import { SensorComponent } from './pages/sensor/sensor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InicioComponent,
+    RegistrarComponent,
+    SensorComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/sensor/sensor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor',
+  templateUrl: './sensor.component.html',
+  styleUrls: ['./sensor.component.css']
+})
+export class SensorComponent {
+
+}
