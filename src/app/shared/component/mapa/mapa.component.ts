618bf3473b8dbdbc588e6e2452bfeeef001f0186
Heidy Olmos <olmoscamposh@gmail.com>
2023-11-23 18:03:24 -0400

    card mapa ok

--- src/app/shared/component/mapa/mapa.component.ts
+++ src/app/shared/component/mapa/mapa.component.ts
@@ -1,27 +1,34 @@
 ///<reference path="../../../../../node_modules/@types/googlemaps/index.d.ts"/>
 
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms'
 import { ElementRef, ViewChild, Renderer2 } from '@angular/core'
+
 
 @Component({
   selector: 'shared-mapa',
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.css']
+
 })
+
+
+
 export class MapaComponent implements OnInit{
 
   @Input()
-  alto: number = 52;
+  alto!: number;
   @Input()
-  ancho: number = 96;
+  ancho!: number;
+  @Input()
+  latitud!: number;
+  @Input()
+  longitud!: number;
 
   @ViewChild('divMap') divMap!: ElementRef;
   @ViewChild('inputPlaces') inputPlaces!: ElementRef;
 
   mapa!: google.maps.Map;
   markers: google.maps.Marker[];
-
 
 
   constructor(private renderer: Renderer2) {
@@ -34,59 +41,17 @@ export class MapaComponent implements OnInit{
   ngAfterViewInit(): void {
 
     const opciones = {
-      enableHighAccuracy: true,
+      enableHighAccuracy: false,
       timeout: 5000,
       maximumAge: 0
     }
 
-    if (navigator.geolocation) {
-
-      navigator.geolocation.getCurrentPosition(async (position) => {
-
-        await this.cargarMapa(position);
-        this.cargarAutocomplete();
-
-      }, null, opciones);
-
-
-    } else {
-      console.log("navegador no compatible")
-    }
-
+    this.cargarMapa({ lat: this.latitud, lng: this.longitud });
   };
 
-
-
-  private cargarAutocomplete() {
-
-    // const autocomplete = new google.maps.places.Autocomplete(this.renderer.selectRootElement(this.inputPlaces.nativeElement), { })
-
-    const autocomplete = new google.maps.places.Autocomplete(this.renderer.selectRootElement(this.inputPlaces.nativeElement), {
-      componentRestrictions: {
-        country: ["CL"]
-      },
-      fields: ["address_components", "geometry", "place_id"],
-      types: ["address"],
-    })
-
-    google.maps.event.addListener(autocomplete, 'place_changed', () => {
-      const place: any = autocomplete.getPlace();
-      console.log("el place completo es:", place)
-
-      this.mapa.setCenter(place.geometry.location);
-      const marker = new google.maps.Marker({
-        position: place.geometry.location
-      });
-
-      marker.setMap(this.mapa);
-    })
-  }
-
-
   cargarMapa(position: any): any {
-
     const opciones = {
-      center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+      center: new google.maps.LatLng(position),
       zoom: 17,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
@@ -94,36 +59,19 @@ export class MapaComponent implements OnInit{
     this.mapa = new google.maps.Map(this.renderer.selectRootElement(this.divMap.nativeElement), opciones)
 
     const markerPosition = new google.maps.Marker({
-      position: this.mapa.getCenter(),
-      title: "David",
+      position: position,
+      title: "Shadow garden",
     });
 
     markerPosition.setMap(this.mapa);
     this.markers.push(markerPosition);
 
+
     google.maps.event.addListener(this.mapa, 'click', (evento: google.maps.MapMouseEvent) => {
-      const marker = new google.maps.Marker({
-        position: evento.latLng,
-        animation: google.maps.Animation.DROP,
-      });
-      marker.setDraggable(true)
-      marker.setMap(this.mapa);
-
-      google.maps.event.addListener(marker, 'click', (event) => {
-        marker.setMap(null);
-
-      })
+      print();
 
     })
-
-
   };
-
-
-
-
-
-
 
 
 
